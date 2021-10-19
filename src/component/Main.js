6f293621fb3f816583a567c9419f035c318ea7e3
usman <mohdusmanali63@gmail.com>
2021-10-20 02:20:48 +0530

    project added

--- src/component/Main.js
+++ src/component/Main.js
@@ -7,10 +7,32 @@ import FormComponent from './FormComponent'
 const Main=()=>{
       const [val,setText]=useState([])
       const [data,setData]=useState([])
-      const [placeholder,setPlaceholder]=useState([
-
-
+      const [example,setExample]=useState([
+            {
+            "name":"userName",
+            "label":"User Name",
+            "fieldType":"TextBox"
+            },
+            {
+            "name":"City",
+            "label":"City",
+            "DataSource":["Bangalore", "Mysore", "Kolkata", "Delhi"],
+            "fieldType":"RadioButton"
+            },
+            {
+            "name":"Gender",
+            "label":"Gender",
+            "fieldType":"DropDown",
+              "DataSource":["Male", "Female"]
+            }
             ])
+      const [placeholder,setPlaceholder]=useState([
+           {
+            "name":"userName",
+            "label":"User Name",
+            "DataSource":["Bangalore", "Mysore", "Kolkata", "Delhi"],
+            "fieldType":"RadioButton"
+            }])
       const addHandler=async()=>{
          try{
              const obj = JSON.parse(val);
@@ -30,12 +52,13 @@ const Main=()=>{
 	
 	return(
          <div className="col-md-10 mx-auto p-5">
-       
+       <h1>Example Json Data </h1>
+       <p>{JSON.stringify(example)}</p>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Enter Valid Json</Form.Label>
                          <Form.Control as="textarea" 
                           onChange={(e)=>setText(e.target.value)} value={val}
-                          placeholder="ss"
+                          placeholder="Valid Json "
                          rows={10} />
                          </Form.Group>
 
